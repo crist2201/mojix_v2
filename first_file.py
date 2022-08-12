d5c2ff150087ffafd49ed5bea5f5d36c6cfb546c
Cristian Salazar Torrez <106119377+crist2201@users.noreply.github.com>
2022-08-11 20:14:47 -0400

    Delete ace

--- first_file.py
+++ first_file.py
@@ -1,8 +1,8 @@
 import streamlit as st
-from streamlit_ace import st_ace
+#from streamlit_ace import st_ace
 
 st.title('10 Cool Beginner Python Tricks That Will Make Your Life Easier')
 
-content = st_ace()
+#content = st_ace()
 
 st.write("Hello Mojix")
