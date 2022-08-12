4310a2482efb5500b5f0da9a428286db7961c61c
Cristian Salazar Torrez <106119377+crist2201@users.noreply.github.com>
2022-08-11 20:59:20 -0400

    Add all tricks

--- first_file.py
+++ first_file.py
@@ -28,7 +28,7 @@ string = '''hello world
 string.split()'''
 st.code(string, language='python')
 st.markdown('**Output**:')
-st.code("['hello','world']",language='python')
+st.success("['hello','world']")
 
 
 
@@ -40,7 +40,7 @@ a=str[::-1]
 print(a)'''
 st.code(stringr, language='python')
 st.markdown('**Output**:')
-st.code("!dlrow olleh",language='python')
+st.success("!dlrow olleh")
 
 
 st.header("4. Merging two dictionaries")
@@ -52,7 +52,7 @@ d3 = {**d1, **d2}
 print(d3)'''
 st.code(dic, language='python')
 st.markdown('**Output**:')
-st.code("{‘a’: 10, ‘b’: 20, ‘c’: 30, ‘d’: 40}",language='python')
+st.success("{‘a’: 10, ‘b’: 20, ‘c’: 30, ‘d’: 40}")
 
 
 st.header("5. The zip() function")
@@ -67,7 +67,7 @@ st.markdown('**Output**:')
 out = '''red apple
 yellow banana
 green mango'''
-st.code(out,language='python')
+st.success(out)
 st.write("The zip() function can also be used for combining two lists into a dictionary. This method can be really helpful while grouping data from the list.")
 st.markdown('**Example**:')
 zip_fun = '''students = [“Rajesh”, “kumar”, “Kriti”]
@@ -76,7 +76,7 @@ dictionary = dict(zip(students, marks))
 print(dictionary)'''
 st.code(zip_fun, language='python')
 st.markdown('**Output**:')
-st.code("{‘Rajesh’: 87, ‘kumar’: 90, ‘Kriti’: 88}",language='python')
+st.success("{‘Rajesh’: 87, ‘kumar’: 90, ‘Kriti’: 88}")
 
 
 
@@ -91,7 +91,7 @@ st.code(var, language='python')
 st.markdown('**Output**:')
 out='''a = 1
 b = [2, 3, 4, 5]'''
-st.code(out,language='python')
+st.success(out)
 st.write("This process is also called list unpacking and you can apply this method for more than 2 variables also!")
 
 
@@ -105,4 +105,52 @@ print(newlist)'''
 st.code(lis, language='python')
 st.markdown('**Output**:')
 out='''{1, 2, 3, 4, 5, 6, 7, 8, 9}'''
-st.code(out,language='python')
+st.success(out)
+
+
+
+st.header("8. Lambda function")
+st.write("If you need a function that is not very complicated, it can be done easily in one line using lambda. They are also called anonymous functions and are used heavily in data science and web development.")
+st.markdown('**Example**:')
+st.write("Let’s say you want to write a function to multiply two numbers. Instead of writing a conventional function, you can do that in one line using :")
+lam = '''mul = lambda a,b: a*b
+mul(5,6)'''
+st.code(lam, language='python')
+st.markdown('**Output**:')
+out='''30'''
+st.success(out)
+
+
+
+st.header("9. Swapping variable value")
+st.write("One of the first programs that we learn while learning about variables is swapping the values of two variables. In python you can achieve that with one line of code:")
+st.markdown('**Example**:')
+var_val = '''a = 100
+b = 200
+a,b = b,a
+print(f’a = ‘,a)
+print(f’b = ‘,b)'''
+st.code(var_val, language='python')
+st.markdown('**Output**:')
+out='''a = 200
+b = 100'''
+st.success(out)
+
+
+
+st.header("10. Use a password in your code")
+st.write("This python trick is amazing for securing your code with a password. We will use the getpass() function from the library getpass which encodes your input. This will prevent anyone from running the code without a password. Isn’t that cool!")
+st.markdown('**Example**:')
+passw = '''from getpass import getpass
+password = getpass(“password: “)
+if password == “abcd”:
+    print(“welcome strnger!”)
+else:
+    print(“wrong password”)'''
+st.code(passw, language='python')
+st.markdown('**Output**:')
+out='''password: **** [abcd]
+Welcome stranger!
+Password: **** [abdc]
+Wrong password'''
+st.success(out)
