15d2f01186b2c6449f77a743d060d816352f8e37
crist2201 <cristiansalazar822@gmail.com>
2022-08-13 11:23:07 -0400

    Test2

--- first_file.py
+++ first_file.py
@@ -2,7 +2,7 @@ import streamlit as st
 #from streamlit_ace import st_ace
 image = "https://www.python.org/static/community_logos/python-logo-master-v3-TM.png"
 
-st.title('14 Cool Beginner Python Tricks That Will Make Your Life Easier')
+st.title('10 Cool Beginner Python Tricks That Will Make Your Life Easier')
 
 col1, col2, col3 = st.columns([1, 6, 1])
 
@@ -16,18 +16,18 @@ with col3:
     st.write("")
 
 # First content
-st.write("The compactness of Python can make a developer’s life a lot easier when writing lines and lines of code. But there are some lesser-known Python tricks that can surprise you with their amazing capabilities. In today’s article, I will discuss 10 Python tips and tricks that will be really helpful for beginners to write more compact code. Knowing these tips and tricks will definitely save you some valuable time.")
+st.text("The compactness of Python can make a developer’s life a lot easier when writing lines and lines of code. But there are some lesser-known Python tricks that can surprise you with their amazing capabilities. In today’s article, I will discuss 10 Python tips and tricks that will be really helpful for beginners to write more compact code. Knowing these tips and tricks will definitely save you some valuable time.")
 
 
 st.header("1. Walrus operator")
 st.write("The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.")
-st.markdown('**Example**:')
+st.markdown('**_Example_**:')
 st.write("If we want to check and print the length of a list:")
 walrus = '''Mylist = [1,2,3]
 if(l := len(mylist) > 2)
 print(l)'''
 st.code(walrus, language='python')
-st.markdown('**Output**:')
+st.markdown('**_Output_**:')
 st.success(3)
 
 
