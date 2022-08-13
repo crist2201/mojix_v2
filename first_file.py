e731bf23c616f249350cb1f8a58473dfcc088ce2
crist2201 <cristiansalazar822@gmail.com>
2022-08-13 11:29:20 -0400

    Finall test

--- first_file.py
+++ first_file.py
@@ -16,7 +16,7 @@ with col3:
     st.write("")
 
 # First content
-st.text("The compactness of Python can make a developer’s life a lot easier when writing lines and lines of code. But there are some lesser-known Python tricks that can surprise you with their amazing capabilities. In today’s article, I will discuss 10 Python tips and tricks that will be really helpful for beginners to write more compact code. Knowing these tips and tricks will definitely save you some valuable time.")
+st.write("The compactness of Python can make a developer’s life a lot easier when writing lines and lines of code. But there are some lesser-known Python tricks that can surprise you with their amazing capabilities. In today’s article, I will discuss 10 Python tips and tricks that will be really helpful for beginners to write more compact code. Knowing these tips and tricks will definitely save you some valuable time.")
 
 
 st.header("1. Walrus operator")
